ba8b3f6b2753057e5571ed39db8c10799a1b0ff7
Koza <kiskoza@gmail.com>
2022-09-10 16:40:18 +0200

    Add offset and width modifier to grid columns

--- app/components/bootstrap/grid/col.rb
+++ app/components/bootstrap/grid/col.rb
@@ -3,10 +3,30 @@
 module Bootstrap
   module Grid
     class Col < ViewComponent::Base
+      attr_accessor :offset
+      attr_accessor :width
+
+      def initialize(width: nil, offset: nil)
+        super
+
+        @offset = offset
+        @width = width
+      end
+
       def call
-        content_tag :div, class: 'col' do
+        content_tag :div, class: "col #{width_modifier} #{offset_modifier}" do
           content
         end
+      end
+
+      private
+
+      def offset_modifier
+        offset ? "offset-#{offset}" : ''
+      end
+
+      def width_modifier
+        width ? "col-#{width}" : ''
       end
     end
   end

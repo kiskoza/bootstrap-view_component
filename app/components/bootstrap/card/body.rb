19e06ef3e48d45799a33b61c6674631d88dc860a
Koza <kiskoza@gmail.com>
2022-09-10 16:21:54 +0200

    Add card body subtitle

--- app/components/bootstrap/card/body.rb
+++ app/components/bootstrap/card/body.rb
@@ -5,9 +5,10 @@ module Bootstrap
     class Body < ViewComponent::Base
       renders_one :text, Bootstrap::ContentTag
       renders_one :title, Bootstrap::ContentTag
+      renders_one :subtitle, Bootstrap::ContentTag
 
       def render?
-        text || title || content
+        text || title || subtitle || content
       end
 
       def before_render
@@ -20,11 +21,17 @@ module Bootstrap
           text.tag ||= :p
           text.classes = ['card-text', *text.classes]
         end
+
+        if subtitle
+          subtitle.tag ||= :h6
+          subtitle.classes = ['card-subtitle', *subtitle.classes]
+        end
       end
 
       def call
         content_tag :div, class: 'card-body' do
           concat(title)
+          concat(subtitle)
           concat(text)
           concat(content)
         end
